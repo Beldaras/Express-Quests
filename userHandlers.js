d499c818a368c0b521da423faddda8f3c9ce2ab5
Beldaras <jphilippe.roblot@gmail.com>
2023-01-23 11:28:48 +0100

    update user done

--- userHandlers.js
+++ userHandlers.js
@@ -31,17 +31,36 @@ const getUserById = (req, res) => {
 }
 
 const postUSer = (req, res) => {
-      const {firstname, lastname, email, ciy, lanquage}=req.body;
+      const {firstname, lastname, email, city, language}=req.body;
 
       database
         .query("INSERT INTO users (firstname, lastname, email, city, language) VALUES (?, ?, ?, ?, ?)",
-          [firstname, lastname, email, ciy, lanquage])
+          [firstname, lastname, email, city, language])
         .then (([result]) => {
           res.location("/api/user/${result.insertId}").sendStatus(201);
         })
         .catch((err) => {
-          console.log(err);
+          console.error(err);
           res.status(500).send("Error saving the user");
         })
 }
-module.exports = { getUsers, getUserById, postUSer};
+
+const updateUser = (req, res) => {
+    const id = parseInt(req.params.id);
+    const {firstname, lastname, email, city, language}=req.body;
+
+    database
+      .query('UPDATE users set firstname = ?, lastname = ?, email = ?, city = ?, language = ? where id = ?', [firstname, lastname, email, city, language, id])
+      .then(([result]) => {
+        if (result.affectedRows === 0){
+          res.status(404).send('Not found');
+        } else {
+          res.sendStatus(204);
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+        res.status(500).send('Error editing the user');
+      })
+}
+module.exports = { getUsers, getUserById, postUSer, updateUser};
